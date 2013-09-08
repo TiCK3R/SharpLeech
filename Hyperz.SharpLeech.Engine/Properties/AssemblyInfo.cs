a9610dccfbb79d2b2f8a3f3876e63cb1587988a5
Hyperz <hyperz.2007@gmail.com>
2013-09-08 13:57:09 +0200

    Bump version to 2.0.1

--- Hyperz.SharpLeech.Engine/Properties/AssemblyInfo.cs
+++ Hyperz.SharpLeech.Engine/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Hyperz")]
 [assembly: AssemblyProduct("SharpLeech 2 C# Engine")]
-[assembly: AssemblyCopyright("Copyright © Hyperz 2008 - 2010")]
+[assembly: AssemblyCopyright("Copyright © Hyperz 2008 - 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.10")]
-[assembly: AssemblyFileVersion("2.0.0.10")]
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
--- Hyperz.SharpLeech/Properties/AssemblyInfo.cs
+++ Hyperz.SharpLeech/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SharpLeech 2.0.0")]
-[assembly: AssemblyDescription("SharpLeech 2.0.0")]
+[assembly: AssemblyTitle("SharpLeech 2.0.1")]
+[assembly: AssemblyDescription("SharpLeech 2.0.1")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Hyperz")]
-[assembly: AssemblyProduct("SharpLeech 2.0.0")]
-[assembly: AssemblyCopyright("Copyright © Hyperz 2008 - 2010")]
+[assembly: AssemblyProduct("SharpLeech 2.0.1")]
+[assembly: AssemblyCopyright("Copyright © Hyperz 2008 - 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -55,5 +55,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.9")]
-[assembly: AssemblyFileVersion("2.0.0.9")]
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
